3c747f89e5fcc25b50d3d4106ff547a2ca1a2c86
Robert Manders <github.powdered589@passmail.net>
2024-12-03 00:51:54 +0000

    Day 2 pass

--- AdventOfCode/Day2.cs
+++ AdventOfCode/Day2.cs
@@ -2,44 +2,47 @@ namespace Advent_of_Code;
 
 public class Day2
 {
+    private static string _direction = "";
+    
     public static int SafeReports(string[] input)
     {
         int safeReports = 0;
         
         foreach (string line in input)
         {
-            List<int> values = [];
-            foreach (string value in line.Trim().Split(' '))
-            {
-                values.Add(int.Parse(value));
-            }
+            List<int> values = ParseLine(line);
 
-            (bool isSafe, List<int> unsafeIndices) = IsSafe(values);
-
-            if (isSafe)
+            if (IsSafe([..values]))
             {
                 safeReports++;
                 continue;
             }
 
-            bool canBeSafe = false;
-            int valuesRemoved = 0;
-            
-            foreach (int index in unsafeIndices)
-            {
-                if (!CanBeSafe([..values], index)) continue;
-                
-                canBeSafe = true;
-                valuesRemoved++;
-            }
-
-            if (canBeSafe && valuesRemoved == 1) safeReports++;
+            if (SafeWithDampener([..values])) safeReports++;
         }
         
         return safeReports;
     }
+
+    private static bool SafeWithDampener(List<int> values)
+    {
+        bool safe = false;
+
+        for (int i = 0; i < values.Count; i++)
+        {
+            List<int> v = RemoveValueFromList([..values], i);
+
+            if (IsSafe(v))
+            {
+                safe = true;
+                break;
+            }
+        }
+
+        return safe;
+    }
     
-    private static bool CanBeSafe(List<int> values, int index)
+    private static List<int> RemoveValueFromList(List<int> values, int index)
     {
         if (index < values.Count)
         {
@@ -47,54 +50,51 @@ public class Day2
         }
         else if (index == values.Count)
         {
-            values = values.Slice(0, index - 1).ToList();
+            values = values[..(index - 1)];
         }
         
-        (bool safe, _) = IsSafe(values);
-        
-        return safe;
+        return values;
     }
 
-    private static (bool isSafe, List<int> unsafeIndices) IsSafe(List<int> values)
+    private static bool IsSafe(List<int> values)
     {
         bool safe = true;
-        string direction = "decreasing";
-
-        List<int> unsafeIndices = [];
-        
-        if (values[1] > values[0])
-        {
-            direction = "increasing";
-        }
+        _direction = values[1] > values[0] ? "increasing" : "decreasing";
         
         for (int i = 1; i < values.Count; i++)
         {
-            if ((direction == "decreasing" && values[i] > values[i - 1]) || (direction == "increasing" && values[i] < values[i - 1]))
+            if (!IsSafeValue(values[i], values[i - 1]))
             {
                 safe = false;
-                unsafeIndices.Add(i);
-                continue;
             }
-            
-            if (Math.Abs(values[i] - values[i - 1]) is >= 1 and <= 3) continue;
-            
-            safe = false;
-            unsafeIndices.Add(i);
         }
-        
-        if ((direction == "decreasing" && values[1] > values[0]) || (direction == "increasing" && values[1] < values[0]))
-        {
-            safe = false;
-            unsafeIndices.Add(0);
-        }
-        
-        int difference = Math.Abs(values[1] - values[0]);
-        if (difference < 1 || difference > 3)
-        {
-            safe = false;
-            unsafeIndices.Add(0);
-        };
 
-        return (safe, unsafeIndices);
+        if (!IsSafeValue(values[1], values[0]))
+        {
+            safe = false;
+        }
+
+        return safe;
+    }
+
+    private static bool IsSafeValue(int i, int j)
+    {
+        if ((_direction == "decreasing" && i > j) || (_direction == "increasing" && i < j))
+        {
+            return false;
+        }
+            
+        return (Math.Abs(i - j) is >= 1 and <= 3);
+    }
+
+    private static List<int> ParseLine(string line)
+    {
+        List<int> values = [];
+        foreach (string value in line.Trim().Split(' '))
+        {
+            values.Add(int.Parse(value));
+        }
+        
+        return values;
     }
 }
--- AdventOfCode_Tests/TestDay2.cs
+++ AdventOfCode_Tests/TestDay2.cs
@@ -7,10 +7,21 @@ public class TestDay2
     [Test]
     public void TestSafeReports()
     {
-        string[] input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Data/Day2/input.txt"));
+        string[] input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Data/Day2/test-input.txt"));
         
         int actual = Day2.SafeReports(input);
         int expected = 4;
+        
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void TestRealSafeReports()
+    {
+        string[] input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Data/Day2/input.txt"));
+        
+        int actual = Day2.SafeReports(input);
+        int expected = 426;
         
         Assert.That(actual, Is.EqualTo(expected));
     }
